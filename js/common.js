c952e7fc46995a9f6aa402df582b17a5ce5361f7
Shimada <ht12a043@oecu.jp>
2022-11-12 20:50:27 +0900

    a

--- js/common.js
+++ js/common.js
@@ -4,4 +4,26 @@
     // 最終更新日を追記
     const lastModified = document.getElementById('lastModified');
     lastModified.textContent += "(最終更新日時：" + dateFomatYYYYMMDDhhmmss(new Date(document.lastModified), '/', ':') + ")";
+
+    // ストレージクラス
+    class Storage {
+        // ストレージ変数
+        #storage;
+        constructor(storage) {
+            this.storage = storage;
+        }
+        setItem(k, v) {
+            this.storage.setItem(k, v);
+        }
+        keyCle(k) {
+            this.storage.removeItem(k);
+        }
+        getItem(k) {
+            return this.storage.getItem(k);
+        }
+    }
+    // ストレージクラスを取得
+    function getStorageOBJ(storage) {
+        return new Storage(storage);
+    }
 }
