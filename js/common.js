61495cb6f9895058e3af19dc0ae322f162f413e3
Shimada <ht12a043@oecu.jp>
2022-11-12 23:27:33 +0900

    a

--- js/common.js
+++ js/common.js
@@ -22,6 +22,35 @@
             return this.storage.getItem(k);
         }
     }
+    // Storageオブジェクトを取得
+    function getStorageOBJ(storage) {
+        return new Storage(storage);
+    }
+
+    // Dexie
+    class Dexie {
+        // DB変数
+        #db;
+        constructor(storage) {
+            this.db = Dexie(config.INDEX_DB.MASTER.DB_NAME);
+            this.db.open().then(function (db) {
+                // 接続に成功したとき
+                console.log("DB connection successful.");
+            }).catch(function (err) {
+                // エラーが起きたとき
+                console.err("DB connection failed. : " + err);
+            });
+        }
+        setItem(k, v) {
+            this.storage.setItem(k, v);
+        }
+        keyCle(k) {
+            this.storage.removeItem(k);
+        }
+        getItem(k) {
+            return this.storage.getItem(k);
+        }
+    }
     // ストレージクラスを取得
     function getStorageOBJ(storage) {
         return new Storage(storage);
