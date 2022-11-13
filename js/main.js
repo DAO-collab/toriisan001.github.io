789eb883997cc64ea6a78905b80a9aee33003dad
Shimada <ht12a043@oecu.jp>
2022-11-13 14:11:30 +0900

    ヘッダーメニューバー追加。上部へ戻るボタン追加

--- js/main.js
+++ js/main.js
@@ -1,27 +1,22 @@
 'use strict';
 {
     // Intersection Observer API
-
-    function callback(entries, obs) {
+    function onScrollCallback(entries) {
         entries.forEach(entry => {
+            // 要素が画面と交差したかどうか判定
             if (!entry.isIntersecting) {
-                return;
+                haderMenuBar.classList.add('scrolled');
+                toTop.classList.add('scrolled');
+            } else {
+                // 要素が画面に表示されている
+                haderMenuBar.classList.remove('scrolled');
+                toTop.classList.remove('scrolled');
             }
-
-            entry.target.classList.add('appear');
-            obs.unobserve(entry.target);
         });
     }
-
-    const options = {
-        threshold: 0.2,
-    };
-
-    const observer = new IntersectionObserver(callback, options);
-
-    const targets = document.querySelectorAll('.animate');
-
-    targets.forEach(target => {
-        observer.observe(target);
-    });
+    const haderMenuBar = document.getElementById('haderMenuBar');
+    const toTop = document.getElementById('to_top');
+    // 特定の要素が画面に交差したときにこちらの callback の処理を実行
+    const onScrollObserver = new IntersectionObserver(onScrollCallback);
+    onScrollObserver.observe(document.getElementById('target'));
 }
