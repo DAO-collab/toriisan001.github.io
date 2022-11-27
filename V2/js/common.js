036f879ce6a967af02fe33614c494d88c9b273c9
Shimada <ht12a043@oecu.jp>
2022-11-27 16:20:05 +0900

    a

--- V2/js/common.js
+++ V2/js/common.js
@@ -18,36 +18,33 @@
         mask.classList.add('hidden');
     });
 
-    // // Intersection Observer API
-    // function onScrollCallback(entries) {
-    //     entries.forEach(entry => {
-    //         // 要素が画面と交差したかどうか判定
-    //         if (!entry.isIntersecting) {
-    //             haderMenuBar.classList.add('scrolled');
-    //             toTop.classList.add('scrolled');
-    //         } else {
-    //             // 要素が画面に表示されている
-    //             haderMenuBar.classList.remove('scrolled');
-    //             toTop.classList.remove('scrolled');
-    //         }
-    //     });
-    // }
-    // const haderMenuBar = document.getElementById('haderMenuBar');
-    // const toTop = document.getElementById('to_top');
-    // // 特定の要素が画面に交差したときにこちらの callback の処理を実行
-    // const onScrollObserver = new IntersectionObserver(onScrollCallback);
-    // onScrollObserver.observe(document.getElementById('target'));
+    // Intersection Observer API
+    function onScrollCallback(entries) {
+        entries.forEach(entry => {
+            // 要素が画面と交差したかどうか判定
+            if (!entry.isIntersecting) {
+                toTop.classList.add('scrolled');
+            } else {
+                // 要素が画面に表示されている
+                toTop.classList.remove('scrolled');
+            }
+        });
+    }
+    const toTop = document.getElementById('pageTop');
+    // 特定の要素が画面に交差したときにこちらの callback の処理を実行
+    const onScrollObserver = new IntersectionObserver(onScrollCallback);
+    onScrollObserver.observe(document.getElementById('target'));
 
-    // // to_topイベント
-    // toTop.addEventListener('click', e => {
-    //     // イベントを無効化(#(パウンド記号がついてしまうことを防ぐため))
-    //     e.preventDefault();
-    //     // ページ上部へ戻る
-    //     window.scrollTo({
-    //         top: 0,
-    //         behavior: 'smooth',
-    //     })
-    // });
+    // pageTopイベント
+    toTop.addEventListener('click', e => {
+        // イベントを無効化(#(パウンド記号がついてしまうことを防ぐため))
+        e.preventDefault();
+        // ページ上部へ戻る
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+    });
 
     // // ストレージクラス
     // class Storage {
