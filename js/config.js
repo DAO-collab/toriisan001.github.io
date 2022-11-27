d5505c21dd4638ff9237e77f5ea815a5e38b1de4
Shimada <ht12a043@oecu.jp>
2022-11-27 17:53:07 +0900

    V2反映

---
+++ js/config.js
@@ -0,0 +1,19 @@
+{
+    'use strict';
+
+    // config返却
+    function getconfig() {
+        return {
+            INDEX_DB: {
+                MASTER: {
+                    DB_NAME: 'ToriiTestDB',
+                    DB_VERSION: 1,
+                },
+                YOUTUBE: {
+                    DB_NAME: 'ToriiTestDB',
+                    DB_VERSION: 1,
+                }
+            }
+        };
+    }
+}
