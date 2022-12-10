2b205fc82b88108397e4c5a4019ac452c311669f
Shimada <ht12a043@oecu.jp>
2022-12-10 22:00:34 +0900

    a

--- js/common.js
+++ js/common.js
@@ -1,75 +1,97 @@
 {
-    'use strict';
+  ("use strict");
 
-    // 画面読み込み前に実行されてエラーになったので100ミリ秒遅延させた。
-    setTimeout(function () {
-        // メニュー制御
-        const open = document.getElementById('open');
-        const menu = document.getElementById('menu');
-        const mask = document.getElementById('mask');
-        open.addEventListener('click', () => {
-            menu.classList.remove('hidden');
-            mask.classList.remove('hidden');
-            // スクロール禁止付与
-            document.addEventListener('touchmove', maskDisableScroll, { passive: false });
-            document.body.classList.add('overflow-hidden');
-        });
-        mask.addEventListener('click', () => {
-            menu.classList.add('hidden');
-            mask.classList.add('hidden');
-            // スクロール禁止除去
-            document.removeEventListener('touchmove', maskDisableScroll, { passive: false });
-            document.body.classList.remove('overflow-hidden');
-        });
+  // 画面読み込み前に実行されてエラーになったので100ミリ秒遅延させた。
+  setTimeout(function () {
+    // メニュー制御
+    const open = document.getElementById("open");
+    const menu = document.getElementById("menu");
+    const mask = document.getElementById("mask");
+    open.addEventListener("click", () => {
+      menu.classList.remove("hidden");
+      mask.classList.remove("hidden");
+      // スクロール禁止付与
+      document.addEventListener("touchmove", maskDisableScroll, {
+        passive: false,
+      });
+      document.body.classList.add("overflow-hidden");
+    });
+    mask.addEventListener("click", () => {
+      menu.classList.add("hidden");
+      mask.classList.add("hidden");
+      // スクロール禁止除去
+      document.removeEventListener("touchmove", maskDisableScroll, {
+        passive: false,
+      });
+      document.body.classList.remove("overflow-hidden");
+    });
 
-        // ページトップへ移動する制御
-        // Intersection Observer API
-        function onScrollCallback(entries) {
-            entries.forEach(entry => {
-                // 要素が画面と交差したかどうか判定
-                if (!entry.isIntersecting) {
-                    toTop.classList.add('scrolled');
-                } else {
-                    // 要素が画面に表示されている
-                    toTop.classList.remove('scrolled');
-                }
-            });
-        }
-        const toTop = document.getElementById('pageTop');
-        // 特定の要素が画面に交差したときにこちらの callback の処理を実行
-        const onScrollObserver = new IntersectionObserver(onScrollCallback);
-        onScrollObserver.observe(document.getElementById('target'));
+    // スクロール禁止(マスク上)
+    function maskDisableScroll(event) {
+      if (event.path[0].id === "mask") {
+        event.preventDefault();
+      }
+    }
 
-        // pageTopイベント
-        toTop.addEventListener('click', e => {
-            // イベントを無効化(#(パウンド記号がついてしまうことを防ぐため))
-            e.preventDefault();
-            // ページ上部へ戻る
-            window.scrollTo({
-                top: -500,
-                behavior: 'smooth',
-            })
-        });
+    // 最終更新日を追記
+    const lastModified = document.getElementById("lastModified");
+    lastModified.textContent +=
+      "(最終更新日時：" +
+      dateFomatYYYYMMDDhhmmss(new Date(document.lastModified), "/", ":") +
+      ")";
 
-        // 最終更新日を追記
-        const lastModified = document.getElementById('lastModified');
-        lastModified.textContent += "(最終更新日時：" + dateFomatYYYYMMDDhhmmss(new Date(document.lastModified), '/', ':') + ")";
+    // 操作パネルの制御
+    const main = document.getElementsByTagName("main")[0];
+    const operationMenu = document.getElementById("operationMenu");
+    const sizeSelect = document.getElementById("sizeSelect");
+    const fontSelect = document.getElementById("fontSelect");
+    const pageSlide = document.getElementById("pageSlide");
+    const pageTop = document.getElementById("pageTop");
+    const pageBottom = document.getElementById("pageBottom");
+    pageSlide.value = 0;
+    pageSlide.max = document.documentElement.scrollHeight;
+    main.addEventListener("click", (e) => {
+      if (!e.path[0].className.match("operationMenuParts")) {
+        operationMenu.classList.toggle("hidden");
+      }
+    });
+    sizeSelect.addEventListener("change", () => {
+      main.style.fontSize = sizeSelect.value;
+      pageSlide.max = document.documentElement.scrollHeight;
+    });
+    fontSelect.addEventListener("change", () => {
+      main.style.fontFamily = fontSelect.value;
+      pageSlide.max = document.documentElement.scrollHeight;
+    });
+    pageSlide.addEventListener("input", () => {
+      window.scrollTo({
+        top: pageSlide.value,
+      });
+    });
+    pageTop.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: -500,
+        behavior: "smooth",
+      });
+    });
+    pageBottom.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: "smooth",
+      });
+    });
 
-        // スクロール禁止(マスク上)
-        function maskDisableScroll(event) {
-            if (event.path[0].id === 'mask') {
-                event.preventDefault();
-            }
-        }
-    
-        // ログを設定
-        // param text ログに設定するテキスト(\nで改行が可能)
-        // return void
-        // use setlog('mvShow');
-        function setlog(text) {
-            const log = document.getElementById('log');
-            log.innerText += `${text}`;
-            console.log(text);
-        }
-    }, 100);
+    // ログを設定
+    // param text ログに設定するテキスト(\nで改行が可能)
+    // return void
+    // use) setlog('mvShow');
+    function setlog(text) {
+        const log = document.getElementById("log");
+        log.innerText += `${text}`;
+        console.log(text);
+      }
+
+  }, 100);
 }
