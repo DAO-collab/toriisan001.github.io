20debad7e24728c4dc9df4c50302f2386cd765b5
Shimada <ht12a043@oecu.jp>
2023-02-04 20:38:19 +0900

    a

--- js/configModule.js
+++
@@ -1,9 +0,0 @@
-export default {
-    SYSTEM_KEY: {
-        LAST_HREF: 'SYSTEM_KEY_LAST_HREF',
-        LOG_SHO_FLAG: 'SYSTEM_KEY_LOG_SHO_FLAG'
-    },
-    PAGE: {
-        HOME: 'https://toriisan001.github.io/page/home.html'
-    },
-};
